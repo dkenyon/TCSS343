ad2546e9a9e80b69c26f57e842891e3dcbb17a30
dkenyon <dkenyon@uw.edu>
2015-05-27 14:24:32 -0700

    Update Assignment5.java
    - Added a random array generator method that populates a passed array with random numbers 0-10.
    - Added method stub for printing to a txt file

--- Assignment5.java
+++ Assignment5.java
@@ -1,17 +1,19 @@
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Testing {
 	public static void main (final String[] theArgs) throws FileNotFoundException {
 		
 		File inFile = new File("src/sample_input.txt");
-		int[][] array = new int[4][4];
-		readFileToArray(inFile, array);
-		printArray(array);
+		int[][] array = new int[10][10];
+		generateRandomArray(array);
+//		readFileToArray(inFile, array);
+		printArrayToConsole(array);
+		writeArrayToFile(array);
 		for (int i = 0; i < array.length -1; i++) {
 			for (int j = 0; j < array.length -1; j++) {
 				if (i > j) {
@@ -26,7 +28,6 @@ public class Testing {
 	//reads from the given file that is and writes it to the specified array
 	private static void readFileToArray(final File theFile, final int[][] theArray) throws FileNotFoundException {
 		Scanner scanner = new Scanner(theFile);
-//		scanner.useDelimiter("	");
 		int currentColumn = 0;
 		int currentRow = 0;
 		while (scanner.hasNext()) {
@@ -51,16 +52,40 @@ public class Testing {
 //				}
 			}
 		}
+		scanner.close();
 	} //end readFileToArray
 	
+	//writes the passed array to a tab-delimited file (if theArray[i][j] == -1, then it is represented with the String "NA")
+	private static void writeArrayToFile(int[][] theArray) throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter("src/input" + theArray.length + ".txt");
+		writer.println("COOL SHIT HERE");
+		writer.println("COOL SHITLINE 2");
+		writer.close();
+	}
+	
+	//populates a given array with random numbers (if i > j, then theArray[i][j] = -1) (if i == j, then theArray[i][j] = 0;)
+	private static void generateRandomArray(int[][] theArray) {
+		Random random = new Random();
+		for (int i = 0; i < theArray.length; i++) {
+			for (int j = 0; j < theArray.length; j++) {
+				if (j == i) {
+					theArray[i][j] = 0;
+				} else if (i > j) {
+					theArray[i][j] = -1;
+				} else {
+					theArray[i][j] = random.nextInt(10);
+				}
+			}
+		}
+	} //end generateRandomArray
+	
 	//prints the array to the console for testing purposes
-	private static void printArray(final int[][] theArray) {
+	private static void printArrayToConsole(final int[][] theArray) {
 		System.out.print("    ");
 		for (int i = 0; i < theArray.length; i++) { //print out column markers
 			System.out.print(i + "    ");
 		}
 		System.out.println();
-		
 		for (int i = 0; i < theArray.length; i++) { //print out row indicators
 			if (i == 0) {
 				System.out.print(i + "   ");
@@ -91,10 +116,4 @@ public class Testing {
 			System.out.println();
 		}
 	} //end print array
-
-	
-	
-
-	
-
 }
